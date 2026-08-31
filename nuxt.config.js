f40cfbe77650b72cb462f893ee47d0ffb5980cbf
elmasha <elmashavitran@gmail.com>
2026-08-31 21:51:49 +0300

    DF

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/logo.png' }
+      { rel: 'icon', type: 'image/x-icon', href: '/' }
     ]
   },
 
